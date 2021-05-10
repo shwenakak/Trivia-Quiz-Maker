93fac5b5142f2109d54b8dfaba601f70f726853e
Arnav-Menon <arnavmenon@gmail.com>
2021-05-09 21:00:27 -0700

    template for quiz class

---
+++ header/quiz.h
@@ -0,0 +1,25 @@
+#ifndef __QUIZ_HPP__
+#define __QUIZ_HPP__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// may need to forward declare Questions class like in lab5
+
+class Quiz {
+
+	private:
+		Questions* q; // reference to Questions class; composite component
+
+	public:
+		// TODO
+		string PickTopic(); // which topic will user choose: Music, Sports, or Science
+		// TODO
+		string QType(); // which questions does user want: T/F, MC, FillinBlank
+		// TODO
+		bool RunGame(); // this will play the game
+};
+
+#endif // __QUIZ_HPP__
