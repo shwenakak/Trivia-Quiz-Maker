bcb4ef715eacfbeeb08f7b4a9d466e373c0dbd9d
Arnav-Menon <arnavmenon@gmail.com>
2021-05-15 15:55:48 -0700

    added function to load questions into vector

--- header/quiz.h
+++ header/quiz.h
@@ -1,26 +1,30 @@
 #ifndef __QUIZ_HPP__
 #define __QUIZ_HPP__
 
-#include <string>
-#include <iostream>
+#include "questions.h"
 
-using namespace std;
+#include <string>
+#include <vector>
 
 // may need to forward declare Questions class like in lab5
 
-class Quiz {
+using namespace std;
 
+class Quiz {
 	private:
 		string topic;
 		string type;
 		Questions* question = nullptr; // reference to Questions class; composite component
 
+		vector<string> MusicQ;
+		vector<string> ScienceQ;
+		vector<string> SportsQ;
+
 	public:
 		~Quiz();
+		void LoadQuestions(string); // fill vectors with questions according to user topic
 		void SetType(string); // param is what type of questions user wants
-		// TODO
 		void PickTopic(); // which topic will user choose: Music, Sports, or Science
-		// TODO
 		void QType(); // which questions does user want: T/F, MC, FillinBlank
 		// TODO
 		bool RunGame(); // this will play the game
