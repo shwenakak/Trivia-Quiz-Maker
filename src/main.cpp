accbf8a3266207b052d1289b7a01d3a0bd6bb5c6
Arnav-Menon <arnavmenon@gmail.com>
2021-05-15 14:47:59 -0700

    main file

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "quiz.cpp"
+#include "../header/questions.hpp"
+
+#include "../header/fill.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	Quiz q;
+
+	q.PickTopic();
+//	q.QType();
+	
+	return 0;
+}
