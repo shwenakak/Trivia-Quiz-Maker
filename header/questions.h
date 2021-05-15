4fce2a609caa257f765d71ba70861e40fa663cab
Arnav-Menon <arnavmenon@gmail.com>
2021-05-15 14:48:55 -0700

    template for questions parent class

---
+++ header/questions.h
@@ -0,0 +1,14 @@
+#ifndef __QUESTIONS_HPP__
+#define __QUESTIONS_HPP__
+
+#include <string>
+
+class Questions {
+	// protected becasue we want subclasses to be able to reference the questions
+	protected:
+		
+	public:
+		
+};
+
+#endif // __QUESTIONS_HPP__
