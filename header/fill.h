a87ac378d2526e4547329849db2ab6a23f7d0dd3
Arnav-Menon <arnavmenon@gmail.com>
2021-05-17 23:12:57 -0700

    added constructor for member variables

--- header/fill.h
+++ header/fill.h
@@ -9,10 +9,17 @@
 using namespace std;
 
 class Fill : public Questions {
-	public:
-		Fill() {} 
+	private:
+		string question;
+		string answer;
 
-		virtual string DisplayQuestion(string) const;
+	public:
+		Fill(string question, string answer) {
+			this->question = question;
+			this->answer = answer;
+		}
+
+		virtual string DisplayQuestion() const;
 		virtual bool CheckAnswer(string, string) const;
 			
 };
