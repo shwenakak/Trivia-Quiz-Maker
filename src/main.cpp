2a817a77361586857fa47d2e2125cf06115dfe66
Arnav-Menon <arnavmenon@gmail.com>
2021-05-25 23:01:26 -0700

    step 1 boiler plate main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "game.cpp"
+#include "../header/fill.hpp"
+//#include "../header/tf.hpp"
+//#include "../header/mc.hpp"
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,8 +12,28 @@ int main() {
 	
 	Game g;
 
-	g.PickTopic();
-	g.QType();
+	string topic = g.PickTopic(); // user will pick either Sports, Science, or Music as a topic
+	
+	g.SetStrategy(new Fill());
+	if (g.LoadQuestion(topic, "fill")) // if 2 fill in the blank questions were appropriately added, change strategy and repeat
+		g.SetStrategy(new TF());
+	else {
+		cout << "Error adding Fill-in-the=Blank Questions" << endl;
+		exit(1);
+	}
+
+	if (g.LoadQuestions(topic, "tf")) // if 2 TF questions were approrpately added, change strategy and repeat
+		g.SetStrategy(new MC());
+	else {
+		cout << "Error adding TF Questions" << endl;
+		exit(1);
+	}
+
+	if (!g.LoadQuestions(topic, "mc")) { // if 2 MC questions were NOT appropriately added, spit ERROR. Else move on
+		cout << "Error adding MC Questions" << endl;
+		exit(1);
+	}
+	
 
 	//if (q.RunGame())
 	//	cout << "CONGRATS! YOU WON!" << endl;
