d666ea1d99e66ba760e71edb9d3368ab31e8fe86
Arnav-Menon <arnavmenon@gmail.com>
2021-05-27 23:33:15 -0700

    mock Option class

---
+++ header/option.hpp
@@ -0,0 +1,27 @@
+#ifndef __OPTION_HPP__
+#define __OPTION_HPP__
+
+#include "game_question.hpp"
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Option : public GameQuestion {
+	private:
+		string option;
+		bool isCorrect;
+
+	public:
+		Option(string o, bool c) : GameQuestion(), option(o), isCorrect(c) { }
+
+		virtual string GetAnswer() const { return "dummy"; }
+		virtual string GetQuestion() const { return "dont need this function in this class"; }
+		virtual int HowManyOptions() const { return -69; }
+		virtual void AddOption(GameQuestion* gq) { cout << "OPTION CLASS" << endl; }
+		virtual void DisplayOptions() { }
+		
+};
+
+#endif // __OPTION_HPP__
