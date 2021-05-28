8a80f135ff7cbc86d59cf3fa3b6f6e54e87d8af1
Arnav-Menon <arnavmenon@gmail.com>
2021-05-27 23:33:50 -0700

    updated to test new stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../header/fill.hpp"
 #include "../header/tf.hpp"
 #include "../header/mc.hpp"
+#include "../header/game_question.hpp"
 
 #include <iostream>
 #include <string>
@@ -13,13 +14,31 @@ int main() {
 	Game g;
 
 	string topic = g.PickTopic(); // user will pick either Sports, Science, or Music as a topic
-	
-	g.SetStrategy(new Fill());
-	g.LoadQuestions(topic, "fill");
-	g.SetStrategy(new TF());
-	g.LoadQuestions(topic, "tf");
-	g.SetStrategy(new MC());
-	g.LoadQuestions(topic, "mc");
+
+//	g.SetStrategy(new Fill());	
+	g.LoadQuestions(topic, "fill"); // load 2 fill-in-blank questions
+	g.LoadQuestions(topic, "tf"); // load 2 tf questions
+	g.LoadQuestions(topic, "mc"); // load 2 mc questions
+
+	int size = g.HowManyQuestions();	
+	cout << size << endl;
+
+//	g.SetStrategy(new Fill());
+	for (int i = 0; i < size; i++) {
+		GameQuestion* q = g.GetQuestion(i);
+		// if q->HowManyOptions() == 4; SetStrategy(new MC())
+		// else if q->HowManyOptions() == 2; SetStrategy(new TF())
+		// else SetStrategy(new Fill())
+		if (q->HowManyOptions() == 4)
+			g.SetStrategy(new MC());
+		else if (q->HowManyOptions() == 2)
+			g.SetStrategy(new TF());
+		else
+			g.SetStrategy(new Fill());
+
+		g.print(q);
+	}	
+
 	
 /*	if (g.LoadQuestions(topic, "fill")) // if 2 fill in the blank questions were appropriately added, change strategy and repeat
 		g.SetStrategy(new TF());
