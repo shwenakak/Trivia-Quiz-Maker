8a3cb0247995e56fc87e2b5a3b97379caaa0181f
Arnav-Menon <arnavmenon@gmail.com>
2021-05-29 12:18:46 -0700

    removed comments

--- header/game_question.hpp
+++ header/game_question.hpp
@@ -21,8 +21,6 @@ class GameQuestion {
 		virtual string GetAnswer() const = 0;
 
 		virtual string GetQuestion() const { return "garbage. need this for Question class"; }
-		// implement in Option class so we can use it in Display(GameQuestion*) function in Type derived classes
-		//virtual vector<GameQuestion*> GetOptions() const = 0;
 		
 		// only need this for Question class but need to define in all subclasses
 		virtual int HowManyOptions() const { return 11; }
