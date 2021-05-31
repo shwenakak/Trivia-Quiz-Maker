13cc05ff528bb69f55e18703fcade93491f98823
github-shwenakak <github-shwenakak@gmail.com>
2021-05-30 18:26:14 -0700

    Option CLass unit tests

---
+++ tests/option_test.hpp
@@ -0,0 +1,27 @@
+#ifndef __OPTION_TEST_HPP__
+#define __OPTION_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "../header/Option.hpp"
+
+TEST(OptionTest, TestCorrectAnswerTrue) {
+  Option o("TestOption", true);
+  EXPECT_DOUBLE_EQ(o.CorrectAnswer(), true);
+}
+
+TEST(OptionTest, TestCorrectAnswerFalse) {
+  Option o("TestOption", false);
+  EXPECT_DOUBLE_EQ(o.CorrectAnswer(), false);
+}
+
+TEST(OptionTest, TestOptionStringTrue) {
+  Option o("TestOption", true);
+  EXPECT_DOUBLE_EQ(o.GetAnswer(), "TestOption");
+}
+
+TEST(OptionTest, TestOptionStringFalse) {
+  Option o("TestOption", false);
+  EXPECT_DOUBLE_NEQ(o.GetAnswer(), "Wrong");
+}
+
+#endif
